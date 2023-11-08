cc0f9bc8cc99763b330ce0264188e2174b47a1ba
naga0026 <150260219+naga0026@users.noreply.github.com>
2023-11-08 15:08:37 -0500

    Delete android directory

--- android/app/src/main/kotlin/com/tjxsbostoreapp/printing/ZebraPrinterData.kt
+++
@@ -1,7 +0,0 @@
-package com.tjxsbostoreapp.printing
-
-import com.zebra.sdk.comm.Connection
-import com.zebra.sdk.printer.ZebraPrinter
-
-class ZebraPrinterData(val printer: ZebraPrinter, var connection: Connection, val ip: String)
-
