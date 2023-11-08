156013e10e42df46ff6a2a10b58ac023a51184d2
naga0026 <150260219+naga0026@users.noreply.github.com>
2023-11-08 15:09:02 -0500

    Delete test directory

--- test/controllers/printer/mock_add_printer_view_controller.dart
+++
@@ -1,6 +0,0 @@
-import 'package:base_project/controller/printer/add_printer_view_controller.dart';
-import 'package:mockito/mockito.dart';
-
-class MockAddPrinterViewController with Mock implements AddPrinterViewController {
-
-}
